77059887a2dc4652ded906d8f10c2dccdd25c7f8
ioleon13 <liya.lhi@gmail.com>
2014-07-31 11:09:22 +0800

    使用匿名包裹器，即自执行匿名函数
    循环中使用闭包，匿名函数保持对外部变量 i 的引用，此时 for循环已经结束， i 的值被修改成了 100.

--- demo/level_me_up/basic_get.js
+++ demo/level_me_up/basic_get.js
@@ -3,12 +3,11 @@ var db = level(process.argv[2]);
 var key = '';
 
 for (var i = 0; i < 101; i++) {
-	key = 'key' + i;
-	(function(key) {
-
+	(function(e) {
+                key = 'key' + e;
 		db.get(key, function(err, value) {
 			console.log(key + '=' + value);
 		});
-	}) (key);
+	}) (i);
 	
 };
