1d2866a8589fade035668134ed5e185651ea5dd9
ioleon13 <liya.lhi@gmail.com>
2014-08-05 21:25:53 +0800

    prints all of the key/value pairs in a LevelDB store to the console.

---
+++ demo/level_me_up/db_streaming.js
@@ -0,0 +1,10 @@
+var level = require('level');
+var db = level(process.argv[2]);
+
+db.createReadStream()
+  .on('data', function(data){
+  	console.log(data.key + '=' + data.value);
+  })
+  .on('error', function(err) {
+  	throw err;
+  })
