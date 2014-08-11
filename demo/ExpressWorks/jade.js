c6ae0637414615a8ae3a8b5e16b79e47563143e7
ioleon13 <liya.lhi@gmail.com>
2014-08-11 22:53:47 +0800

    An Express.js app with a home page (/home) rendered by jade template engine.

---
+++ demo/ExpressWorks/jade.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.set('views', process.argv[3]);
+app.set('view engine', 'jade');
+
+app.get('/home', function(req, res) {
+	res.render('index', {date: new Date().toDateString()});
+});
+app.listen(process.argv[2]);
