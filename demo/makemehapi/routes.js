b237abd80a8fada6dcdeb4b17424d63158826ee9
ioleon13 <liya.lhi@gmail.com>
2014-09-02 23:37:47 +0800

    Modify the port number
    the default port number was 8080

--- demo/makemehapi/routes.js
+++ demo/makemehapi/routes.js
@@ -1,6 +1,6 @@
 var hapi = require('hapi');
 
-var server = hapi.createServer('localhost', 8080);
+var server = hapi.createServer('localhost', Number(process.argv[2] || 8080));
 
 var getName = function(request, reply) {
   reply('Hello ' + request.params.name);
